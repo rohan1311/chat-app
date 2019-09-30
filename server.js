9c9214bf4fe6d2c86bf2193477e6d6f3c4005b4a
rohan1311 <rohan.singh@sap.com>
2019-09-30 14:22:55 +0530

    only commit

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var urlencodedParser = bodyParser.urlencoded({extended : false})
+var app = express();
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+var messages=[]
+var users=[]
+
+app.get('/', (req, res) => {
+   res.sendFile('test.html', {root: __dirname })
+})
+
+app.post('/send_message', urlencodedParser, (req,res) => {
+   messages.push(req.body)
+   io.emit('message')
+   res.sendStatus(200)
+})
+
+app.get('/messages', (req,res)=>{
+   res.send(JSON.stringify(messages))
+})
+
+io.on('connection', (socket)=>{
+   console.log('a user connected')
+   socket.on('setUserName', (data)=>{
+      if(users.indexOf(data)==-1){
+         users.push(data)
+         socket.emit('userSet', {username:data})
+      }else{
+         console.log('server mai')
+         socket.emit('userExists', `${data} already exists, please change your Username to join the chat`)
+      }
+   })
+   socket.on('typing', (data)=>{
+      if(data.typing==true)
+         io.emit('display', data)
+      else
+         io.emit('display', data)
+   })
+}) 
+
+const port = process.env.PORT || 3000
+var server = http.listen(port, () => {
+   console.log(`app listening at port ${server.address().port}`)
+})
+
+
+
